9aac0c4ac3101d514057fa482935c01c8ead2a02
Sowmdo0968 <sowmkb@yahoo.com>
2017-03-18 23:51:14 -0400

    my solution

--- 4.DataStructure/300102828/src/solutions.scala
+++ 4.DataStructure/300102828/src/solutions.scala
@@ -3,8 +3,14 @@
   */
 object  solutions {
 
-  def main( args: Array[String] ): Unit ={
-  println("hello world\n")
+  def main(args: Array[String]): Unit = {
+    println("hello world\n")
   }
 
+}
+class WaterPouring (capacities: Vector[Int]) {
+  val a = 9
+  def solutions (x:Int): List[Int]={
+    Nil
+  }
 }
