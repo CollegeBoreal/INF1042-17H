f800610be19cf679fad57a6a86e963d4d0fcbaa0
Sowmdo0968 <sowmkb@yahoo.com>
2017-03-28 14:47:06 -0400

    solution

--- 4.DataStructure/300102828/src/solutions.scala
+++ 4.DataStructure/300102828/src/solutions.scala
@@ -12,5 +12,24 @@ class WaterPouring (capacities: Vector[Int]) {
   val a = 9
   def solutions (x:Int): List[Int]={
     Nil
+
+
   }
+}
+
+object Solutions {
+  def main(args: Array[String]): Unit={
+    val problem = new WaterPouring(Vector(9,4))
+    problem.solutions(6).foreach(println)
+    type state = Vector[Int]
+    val initialState = (0,0)
+    trait Move
+    def changeState = (9,0)
+    trait gMove
+    def changeState = (5,4)
+      
+
+
+  }
+
 }
