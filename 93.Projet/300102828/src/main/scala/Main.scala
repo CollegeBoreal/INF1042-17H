23348b6d0ba6c53b5628bef23a82afd4b8377471
Sowmdo0968 <sowmkb@yahoo.com>
2017-04-17 12:01:59 -0400

    mon projet

--- 93.Projet/300102828/src/main/scala/Main.scala
+++ 93.Projet/300102828/src/main/scala/Main.scala
@@ -3,3 +3,12 @@ object Main{
 
   }
 }
+def caisse (quantite: Int, prix: Int): Unit = {
+  val tax : Double = 0.13
+  if(quantite == 0) return 0
+  else return quantite*prix + tax*(quantite*prix)
+
+
+
+}
+println("le prix est:" + caisse)
