556b0dc7ad73e99f9f29e9892ef9b94ef846ac2c
Sowmdo0968 <sowmkb@yahoo.com>
2017-04-18 16:23:47 -0400

    projet

--- 93.Projet/300102828/src/main/scala/Main.scala
+++ 93.Projet/300102828/src/main/scala/Main.scala
@@ -1,14 +1,12 @@
+import caisse.Tax
+
 object Main{
   def main(args: Array[String]): Unit = {
-
+    println("le recu est:" + Tax.caisse(quantite = 2,prix = 5))
   }
 }
-def caisse (quantite: Int, prix: Int): Unit = {
-  val tax : Double = 0.13
-  if(quantite == 0) return 0
-  else return quantite*prix + tax*(quantite*prix)
 
 
 
-}
-println("le prix est:" + caisse)
+
+
