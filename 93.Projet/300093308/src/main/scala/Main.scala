eabc193566f368b45635173c9ab030f1d537e681
GulKK <putuville@hotmail.com>
2017-04-19 14:41:27 -0400

    Tri

--- 93.Projet/300093308/src/main/scala/Main.scala
+++ 93.Projet/300093308/src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
-
+val resultat = (Tri.tri_bulle(tableau = Array(4,5,6,3,1)))
+    resultat.foreach(x => print(x))
+  }
+}
+object Tri{
+  def tri_bulle(tableau: Array[Int]): Array [Int]= {
+    Array(1,2,5,3,6)
   }
 }
