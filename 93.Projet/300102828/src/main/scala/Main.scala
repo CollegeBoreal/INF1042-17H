585a210e2e65859a92f7a50738696c8dc6bc6512
Sowmdo0968 <sowmkb@yahoo.com>
2017-04-26 21:09:35 -0400

    Projet Final

--- 93.Projet/300102828/src/main/scala/Main.scala
+++ 93.Projet/300102828/src/main/scala/Main.scala
@@ -4,7 +4,7 @@ object Main{
   def main(args: Array[String]): Unit = {
     println("la monait est:" + Tax.caisse(quantite = 2,prix = 5))
     println("la valeur est " + Bits.lastBit(Bits.BOTTOM_ROW))
-    println("le nombre est " + Bits.count(Bits.TOP_ROW))
+    println("le nombre est " + Bits.count(Bits.CAPTURED))
     println("la valeur est " + Bits.at(1,9))
   }
 }
